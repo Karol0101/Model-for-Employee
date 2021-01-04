d87da7651e1a62b51ceda09576374eb196ab93f5
Karol0101 <76934603+Karol0101@users.noreply.github.com>
2021-01-04 17:32:32 +0545

    Add files via upload

--- models.py
+++ models.py
@@ -1,11 +1,25 @@
 from django.db import models
+from django.utils import timezone
+import re
+from django.core.validators import EmailValidator, MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+
+# class shoes_details:
+#   shoes_brand = models.TextField(max_length=50)
+#  shoes_size = models.IntegerField(primary_key=True)
+# shoes_color = models.TextField(max_length=50)
+#shoes_type = models.TextField(max_length=50)
 
 
-class Destination(models.Model):
-    name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='pics')
-    desc = models.TextField()
-    price = models.IntegerField()
-    offer = models.BooleanField(default=False)
+class customer:
+    cust_name = models.TextField(max_length=50)
+    cust_address = models.TextField(max_length=50)
+    cust_number = models.IntegerField(max_length=50)
+    email = models.EmailField(max_length=50, validators=[EmailValidator(
+    ), MinLengthValidator(7), MaxLengthValidator(100)])
+    order_time = models.DateTimeField(default=timezone.now, null=False)
+
+
+class Meta:
+    db_table = 'customer'
+    ordering = ['order_time']
